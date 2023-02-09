37d231d9eb7311ad8a03e710590b82588f5823e5
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2023-02-08 23:36:00 -0300

    conteudo da aula 4

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,32 +1,60 @@
-import { Button, Card, CardActions, CardContent } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Input, Stack } from "@mui/material";
 import Grid from "../components/Grid";
 import styled from "@emotion/styled";
 
 const Login = () => {
+  const year = (new Date()).getFullYear()
   return (
     <Grid>
       <Login.Container>
-        <Card>
+        <Login.Cliente>
+          Nossa Obra
+        </Login.Cliente>
+        <Login.Card>
           <CardContent>
-            <label htmlFor="">E-mail</label>
-            <input type="email" />
-            <label htmlFor="">Senha</label>
-            <input type="password" />
+            <Stack spacing={2}>
+              <label>E-mail</label>
+              <Input />
+              <label>Senha</label>
+              <Input type="password" />
+            </Stack>
           </CardContent>
           <CardActions>
-            <Button>
+            <Button variant="contained" fullWidth size="large">
               Entrar
             </Button>
           </CardActions>
-        </Card>
+        </Login.Card>
+        <Login.Sistema>
+          Sistema desenvolvido em {year}
+        </Login.Sistema>
       </Login.Container>
     </Grid>
   )
 }
 
+Login.Card = styled(Card)`
+  border-radius: ${p => p.theme.spacing(2)};
+  padding: ${p => p.theme.spacing(2)};
+`
+
 Login.Container = styled.div`
+  display: flex;
+  flex-direction: column;
   margin-top: ${p => p.theme.spacing(12)};
-  padding: ${p => p.theme.spacing(2)}
+  padding: ${p => p.theme.spacing(2)};
+  gap: ${p => p.theme.spacing(2)};
+`
+
+Login.Cliente = styled.div`
+  text-align: center;
+  font-size: ${p => p.theme.typography.h4.fontSize};
+  color: ${p => p.theme.palette.primary.main};
+`
+
+Login.Sistema = styled.div`
+  font-size: 0.8rem;
+  text-align: center;
 `
 
 export default Login
